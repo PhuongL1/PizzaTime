96904f9daa8b717789068a3e76ed23a1fd19802a
N1ght <vphuong4725@gmail.com>
2026-07-04 15:04:47 +0700

    fix(task-20260702-43a): configure Cloudinary upload

--- app/src/main/java/com/devpro/pizzatime/feature/admin/menu/CloudinaryConfig.kt
+++ app/src/main/java/com/devpro/pizzatime/feature/admin/menu/CloudinaryConfig.kt
@@ -1,13 +1,11 @@
 package com.devpro.pizzatime.feature.admin.menu
 
 object CloudinaryConfig {
-    const val CLOUD_NAME = "ASK_ME_TO_FILL"
-    const val UPLOAD_PRESET = "ASK_ME_TO_FILL"
+    const val CLOUD_NAME = "zdyxyf4h"
+    const val UPLOAD_PRESET = "pizzatime_products"
     const val FOLDER = "pizzatime/products"
 
     val isConfigured: Boolean
         get() = CLOUD_NAME.isNotBlank() &&
-            UPLOAD_PRESET.isNotBlank() &&
-            CLOUD_NAME != "ASK_ME_TO_FILL" &&
-            UPLOAD_PRESET != "ASK_ME_TO_FILL"
+            UPLOAD_PRESET.isNotBlank()
 }
